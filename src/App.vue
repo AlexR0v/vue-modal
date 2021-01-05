84f74a220f91394343a093247a31bbea79c52423
AlexR0v <mail.ponomarev@yandex.ru>
2021-01-05 12:28:59 +0300

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,35 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <div class='container'>
+    <button
+        class='btn btn_primary'
+        @click='firstModal = !firstModal'
+    >Modal First
+    </button>
+    <Modal
+        v-if='firstModal'
+        title='My Modal'
+    />
   </div>
 </template>
 
-<script lang="ts">
-import Vue from 'vue';
-import HelloWorld from './components/HelloWorld.vue';
+<script lang='ts'>
+import Vue from 'vue'
+import Modal from '@/components/Modal.vue'
 
 export default Vue.extend({
   name: 'App',
-  components: {
-    HelloWorld
+  components: {Modal},
+  data() {
+    return {
+      firstModal: false
+    }
   }
-});
+})
 </script>
 
-<style lang="scss">
-#app {
+<style lang='scss'>
+body {
   font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
---
+++ src/components/Modal.vue
@@ -0,0 +1,79 @@
+<template>
+  <div class='modal__wrapper'>
+    <div class='modal-content'>
+
+      <div class='modal-header'>
+        <div class='modal-title'>{{ title }}</div>
+        <div class='btn-close'>&#10005;</div>
+      </div>
+
+      <div class='modal-body'>
+
+      </div>
+
+    </div>
+  </div>
+</template>
+
+<script lang='ts'>
+import Vue from 'vue'
+
+export default Vue.extend({
+  name: 'Modal',
+  props: {
+    title: {
+      type: String,
+      required: true
+    }
+  }
+})
+</script>
+
+<style
+    lang='scss'
+    scoped
+>
+
+.modal__wrapper {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  position: fixed;
+  top: 0;
+  left: 0;
+  bottom: 0;
+  right: 0;
+  transition: opacity .3s ease;
+  z-index: 998;
+  background-color: rgba(0, 0, 0, .48);
+}
+
+.modal-content{
+  position: relative;
+  max-width: 600px;
+  padding: 20px 18px;
+  background-color: #fff;
+  border: 1px solid #dcdfe6;
+  transition: all .3s ease;
+  border-radius: 8px;
+  z-index: 999;
+  overflow: hidden;
+}
+
+.modal-header{
+  display: flex;
+  align-self: center;
+  justify-content: space-between;
+  padding-bottom: 20px;
+  span{
+    font-size: 20px;
+  }
+  .btn-close{
+    cursor: pointer;
+  }
+}
+
+.modal-body{
+  text-align: center;
+}
+</style>
